3d5aa297cb3860b87f1d446af29b0cc1208930d9
CrashedBboy <crashedbboy@gmail.com>
2015-11-25 07:53:17 +0800

    js layout

---
+++ js/class.js
@@ -0,0 +1,30 @@
+'use strict';
+
+function update(){};
+
+/** share this image as blob (to wallpaper manager) */
+function setWallpaper(){};
+
+/** get daily list of wallpapers */
+function retrieveList(){};
+
+/** append one of wallpaper list on app body */
+function preview(){};
+
+/** show loading icon on button */
+function loading(){};
+
+/** show status bar */
+function showStatus(){};
+
+/** append message on status bar */
+function message(msg){};
+
+/** hide status bar and show apply button */
+function showApplyBtn(){};
+
+/** 'Get New' on main button */
+function showUpdateBtn(){};
+
+/** */
+function checkNetwork(){};

fb7b45e2c979e04612a1195629292c98b24184c2
CrashedBboy <crashedbboy@gmail.com>
2015-12-03 16:19:01 +0800

    apply each function on DOM click event

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 'use strict';
+loading();
+$(document).ready(function(){
+	showUpdateBtn();
+	showStatus("<< Click to update");
+	$("#new_btn_wrapper").click(update);
+	$("#apply_btn").click(setWallpaper);
+});
